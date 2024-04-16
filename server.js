c8632145822a6b4273cd315e962795bfab75984c
Jimzical <arishkumar2003@gmail.com>
2024-04-16 23:40:13 +0530

    Refactor server.js and game.html

--- server.js
+++ server.js
@@ -7,50 +7,32 @@ const WebSocket = require('ws');
 const wss = new WebSocket.Server({ server: app.listen(port) });
 console.log(`Server is running on http://localhost:${port}`);
 
-let buttonPressed = false;
 let counter = 0;
 
-// Serve the main website
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Serve the phone page
 app.get('/phone', (req, res) => {
   res.sendFile(path.join(__dirname, 'phone.html'));
 });
 
-// Serve the game page
 app.get('/game', (req, res) => {
   res.sendFile(path.join(__dirname, 'game.html'));
 });
 
-// Handle WebSocket connections
 wss.on('connection', (ws) => {
   console.log('New WebSocket connection');
+  ws.send(JSON.stringify({ counter }));
 
-  // Send the current button press state and counter value to the client
-  ws.send(JSON.stringify({ buttonPressed, counter }));
-
-  // Listen for messages from the client
   ws.on('message', (message) => {
     const data = JSON.parse(message);
 
-    if (data.buttonPressed !== undefined) {
-      buttonPressed = data.buttonPressed;
-      if (buttonPressed) {
-        counter++;
-      } else {
-        counter = 0;
+    // Broadcast the received data to all connected clients
+    wss.clients.forEach((client) => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message);
       }
-      console.log(`Button press state updated: ${buttonPressed}, Counter: ${counter}`);
-
-      // Broadcast the updated button press state and counter value to all connected clients
-      wss.clients.forEach((client) => {
-        if (client !== ws && client.readyState === WebSocket.OPEN) {
-          client.send(JSON.stringify({ buttonPressed, counter }));
-        }
-      });
-    }
+    });
   });
 });
