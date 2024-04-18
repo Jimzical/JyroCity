6ba9405a23a4dc8c40467c6fb8ba48c7a12facc5
Jimzical <arishkumar2003@gmail.com>
2024-04-18 17:29:44 +0530

    made a proper js for each file

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ wss.on('connection', (ws) => {
   console.log('New WebSocket connection from server.js');
 
   // Send the current button press state and counter value to the client
-  ws.send(JSON.stringify({ buttonPressed, counter }));
+  // ws.send(JSON.stringify({ buttonPressed, counter }));
 
   // Listen for messages from the client
   ws.on('message', (message) => {
