0d609cf8e5c5ba489887b1fe4c444b8d569dd810
Jimzical <arishkumar2003@gmail.com>
2024-06-01 22:18:28 +0530

    chore: Update .gitignore and ngrokRunner.bat

--- scripts/ngrokRunner.bat
+++ scripts/ngrokRunner.bat
@@ -1,9 +1,7 @@
 @echo off
 
-REM Set the path to the Ngrok executable
-set "ngrok_path=C:\Users\arish\Downloads\ngrok\ngrok.exe"
+REM Read the path to the Ngrok executable from ngrokpath.txt
+set /p ngrok_path=<scripts/ngrokpath.txt
 
 REM Start Ngrok with the specified path and port in the background
 start /MIN "" "%ngrok_path%" http 8000
-
-
